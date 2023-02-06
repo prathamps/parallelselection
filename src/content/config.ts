d01394fd0b0446b1077db20829d8d10befa71ec6
prathamps <pspratham16@gmail.com>
2023-02-06 22:21:37 +0530

    Added content collection and dynamic routing

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content"
+// 2. Define a schema for each collection you'd like to validate.
+export const collection = {
+	blog: defineCollection({
+		schema: z.object({
+			draft: z.boolean().default(false),
+			date: z.date().transform((str) => new Date(str)),
+			title: z.string(),
+			slug: z.string().optional(),
+			category: z.enum(["tech", "food"]),
+			tags: z.array(z.string()).optional(),
+			description: z.string(),
+			share: z
+				.object({
+					image: z.string().optional(),
+					title: z.string(),
+					description: z.string(),
+				})
+				.strict(),
+		}),
+	}),
+}
