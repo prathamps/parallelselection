01951d847d15de97c193b6b0bb3fcd6b0534f7d1
prathamps <pspratham16@gmail.com>
2023-04-23 01:31:49 +0530

    Set up url grabbing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,18 @@
 const searchInput = document.querySelector("[data-search]")
+let searchData
 searchInput.addEventListener("input", (e) => {
 	const value = e.target.value
-	console.log(value)
+	searchData = value
 })
+
+addEventListener("keypress", (event) => {
+	if (event.key === "Enter") {
+		event.preventDefault()
+	}
+})
+
+const getSearchData = () => searchData
+
+console.log("searchData", searchData)
+
+export { searchData }
